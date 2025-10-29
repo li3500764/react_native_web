2d6a64933db753aab37813cad038a72fc6b67400
li3500764 <820901797@qq.com>
2025-10-29 09:17:15 +0800

    feat：完成第一个页面的样式部分

---
+++ services/useFetch.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+  console.log('useFetch');
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+        console.log(err);
+      setError(err instanceof Error ? err : new Error("Unknown error"));
+    } finally {
+      setLoading(false);
+    }
+  };
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
